b62d97f54e4454a3959b0ddb563894f475eb0046
pyatnicyn.stanislav <pyatnicyn.stanislav@yandex.ru>
2020-09-16 16:05:28 +0300

    Created MapActivity; Added location determination;

---
+++ app/src/main/java/com/spappstudio/spnavifator/MapActivity.java
@@ -0,0 +1,168 @@
+package com.spappstudio.spnavifator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.GeoObjectCollection;
+import com.yandex.mapkit.MapKit;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.ObjectEvent;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateSource;
+import com.yandex.mapkit.map.CompositeIcon;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.VisibleRegionUtils;
+import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.search.Response;
+import com.yandex.mapkit.search.SearchFactory;
+import com.yandex.mapkit.search.SearchManager;
+import com.yandex.mapkit.search.SearchManagerType;
+import com.yandex.mapkit.search.SearchOptions;
+import com.yandex.mapkit.search.Session;
+import com.yandex.mapkit.user_location.UserLocationLayer;
+import com.yandex.mapkit.user_location.UserLocationObjectListener;
+import com.yandex.mapkit.user_location.UserLocationView;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
+
+public class MapActivity extends AppCompatActivity implements Session.SearchListener, CameraListener, UserLocationObjectListener {
+
+    public static final String MAPKIT_API_KEY = "your key";
+
+    MapView mapView;
+    EditText editTextSearch;
+    SearchManager searchManager;
+    Session searchSession;
+    UserLocationLayer userLocationLayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        SearchFactory.initialize(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.COMBINED);
+
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().addCameraListener(this);
+        mapView.getMap().setRotateGesturesEnabled(false);
+
+        MapKit mapKit = MapKitFactory.getInstance();
+        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
+        userLocationLayer.setVisible(true);
+        userLocationLayer.setHeadingEnabled(true);
+        userLocationLayer.setObjectListener(this);
+
+        editTextSearch = (EditText)findViewById(R.id.edit_text_search);
+        editTextSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (i == EditorInfo.IME_ACTION_SEARCH) {
+                    submitQuery(editTextSearch.getText().toString());
+                }
+                return false;
+            }
+        });
+
+        mapView.getMap().move(
+                new CameraPosition(new Point(0, 0), 14.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+
+        submitQuery(editTextSearch.getText().toString());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+    private void submitQuery(String query) {
+        if (!query.isEmpty()) {
+            searchSession = searchManager.submit(
+                    query,
+                    VisibleRegionUtils.toPolygon(mapView.getMap().getVisibleRegion()),
+                    new SearchOptions(),
+                    this
+            );
+        }
+    }
+
+    @Override
+    public void onSearchResponse(@NonNull Response response) {
+        MapObjectCollection mapObjectCollection = mapView.getMap().getMapObjects();
+        mapObjectCollection.clear();
+
+        for (GeoObjectCollection.Item searchResult : response.getCollection().getChildren()) {
+            Point resultLocation = searchResult.getObj().getGeometry().get(0).getPoint();
+            if (resultLocation != null) {
+                mapObjectCollection.addPlacemark(
+                        resultLocation,
+                        ImageProvider.fromResource(this, R.drawable.search_result)
+                );
+            }
+        }
+    }
+
+    @Override
+    public void onSearchError(@NonNull Error error) {
+        String errorMessage = getString(R.string.unknown_error_message);
+        if (error instanceof RemoteError) {
+            errorMessage = "RemoteError";//getString(R.string.remote_error_message);
+        } else if (error instanceof NetworkError) {
+            errorMessage = "NetworkError";//getString(R.string.network_error_message);
+        }
+
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCameraPositionChanged(@NonNull Map map, @NonNull CameraPosition cameraPosition, @NonNull CameraUpdateSource cameraUpdateSource, boolean b) {
+        if(b) {
+            submitQuery(editTextSearch.getText().toString());
+        }
+    }
+
+    @Override
+    public void onObjectAdded(@NonNull UserLocationView userLocationView) {
+        userLocationLayer.setAnchor(
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
+                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
+
+                userLocationView.getArrow().setIcon(ImageProvider.fromResource(this, R.drawable.user_arrow));
+    }
+
+    @Override
+    public void onObjectRemoved(UserLocationView view) {
+    }
+
+    @Override
+    public void onObjectUpdated(UserLocationView view, ObjectEvent event) {
+    }
+}
